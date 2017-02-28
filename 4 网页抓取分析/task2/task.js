2f60795b524d8a744e5c8c60f547baae7a21b393
materialcoder <1028106985@qq.com>
2017-02-28 11:08:01 +0800

    add comment

--- 4 网页抓取分析/task2/task.js
+++ 4 网页抓取分析/task2/task.js
@@ -1,55 +1,46 @@
 var page = require('webpage').create(),
 	system = require('system'),
+	fs = require('fs'),
 	device=system.args[1],
 	keyword = system.args[2],
 	url = "http://www.baidu.com/s?wd=" + keyword,
 	dataList = [],
 	result = {},
-	w,h,
-	configs = [
-		{
-			name: 'iphone5',
-			userAgent:'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
-			width:320,
-			height:568
-		},
-		{
-			name: 'iphone6',
-			userAgent:'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
-			width:375,
-			height:667
-		},
-		{
-			name: 'ipad',
-			userAgent:'Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
-			width:768,
-			height:1024
-		}
-	];
+	w,h;
 
-// 判断设备
-for(var i in configs) {
-	if(configs[i].name == device){
-		page.settings.userAgent = configs[i].userAgent;
-		page.settings.viewportSize = {width:configs[i].width, height:configs[i].height}
-		w = configs[i].width;
-		h = configs[i].height;
+//读取配置文件
+if(fs.exists('config.json')) {
+	var file = fs.open('config.json','r'),
+		content = '', config = null;
+	while(!file.atEnd()) {
+		content += file.readLine();  //string
 	}
+	config = JSON.parse(content); //object 从一个字符串中解析出json对象
+	for(var i = 0 ; i<3;i++){
+		if (config[i].name === device) {
+			deviceConf = config[i];
+		}
+	}
+	page.settings.userAgent = deviceConf.userAgent;
+	page.viewportSize = {width:deviceConf.width, height:deviceConf.height};
+	w = deviceConf.width;
+	h = deviceConf.height;
 }
 
 var	startTime = Date.now();
 
+//抓取页面信息
 page.open(url,function(status){
 	if(status !== 'success') {
 		console.log('Fall to load the address');
 		result.code = 0;
 		result.msg = '抓取失败';
-		console.log(JSON.stringify(result));
+		console.log(JSON.stringify(result)); //从一个对象解析出字符串
 		phantom.exit();
 	} else {
 		console.log('页面打开成功，正在抓取信息中...');
 
-		// 抓取页面
+		// 查找页面信息
 		page.includeJs('./jquery-3.1.0.min.js',function(){
 			dataList = page.evaluate(function() {
 				var data = [];
@@ -65,13 +56,14 @@ page.open(url,function(status){
 				return data;
 			});
 
+			//回显结果
 			result = {
 				code:1,
 				msg:'抓取成功',
-				device:device,
-				word:keyword,
-				time:Date.now()-startTime,
-				dataList:dataList
+				device:device, //设备名称
+				word:keyword, //搜索关键词
+				time:Date.now()-startTime, //运行时间
+				dataList:dataList //抓取的信息
 			}
 
 			console.log(JSON.stringify(result));
@@ -79,7 +71,7 @@ page.open(url,function(status){
 
 		// 截取屏幕
 		setTimeout(function() {
-			page.clipRect = {top:0, left:0, width:w, height:h};
+			page.clipRect = {top:0, left:0, width:w, height:h}; //截取屏幕的大小,默认为截取整个页面的大小
 			page.render(device + '.png');
 			phantom.exit();
 		},2000);
